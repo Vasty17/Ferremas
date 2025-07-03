a8e6d82ba987cbe06b3aaab0850f496efa759ba9
Vasty17 <basti.gonzalezp@duocuc.cl>
2025-07-03 01:24:49 -0400

    backend y frontend

--- ferremas/src/main/java/com/matiassalinas/ferremas/repositories/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.matiassalinas.ferremas.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.matiassalinas.ferremas.entities.Pedido;
-
-// Esta interfaz permite hacer operaciones como guardar, buscar y eliminar pedidos
-public interface PedidoRepository extends CrudRepository<Pedido, Long> {
-}
